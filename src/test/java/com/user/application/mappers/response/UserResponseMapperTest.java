f960a67fc025d6cfca20da8ad59ba4cbe61ce3a5
melissasoledade <melissa.gsoledade@gmail.com>
2025-09-28 18:50:10 -0300

    adjusting user response mapper

--- src/test/java/com/user/application/mappers/response/UserResponseMapperTest.java
+++ src/test/java/com/user/application/mappers/response/UserResponseMapperTest.java
@@ -3,7 +3,10 @@ package com.user.application.mappers.response;
 import com.user.application.dto.response.UserResponseDTO;
 import com.user.domain.entities.User;
 import com.user.fixtures.application.response.UserResponseDTOHelper;
+import com.user.fixtures.domain.AddressHelper;
 import com.user.fixtures.domain.UserHelper;
+import com.user.fixtures.domain.UserPreferencesHelper;
+import com.user.fixtures.domain.UserProfilesHelper;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.mockito.junit.jupiter.MockitoExtension;
@@ -12,7 +15,7 @@ import org.springframework.boot.test.context.SpringBootTest;
 
 import java.text.ParseException;
 
-import static org.junit.jupiter.api.Assertions.*;
+import static org.junit.jupiter.api.Assertions.assertEquals;
 
 @SpringBootTest
 @ExtendWith(MockitoExtension.class)
@@ -33,15 +36,25 @@ class UserResponseMapperTest {
     @Test
     void shouldMapUserEntityToUserResponseDTO() throws ParseException {
         // given
-        final User user = UserHelper.defaultUser().build();
+        final User user = UserHelper.defaultUser()
+                .userProfiles(UserProfilesHelper.defaultUserProfiles().build())
+                .userPreferences(UserPreferencesHelper.defaultUserPreferences().build())
+                .address(AddressHelper.defaultAddress().build())
+                .build();
         final UserResponseDTO userResponseDTO = UserResponseDTOHelper
                 .defaultUserResponseDTO().build();
 
         // when
         final UserResponseDTO result = this.userResponseMapper.fromUser(user);
 
-        assertEquals(4L, result.getUserId());
-        assertEquals("", result.getUserProfiles().getProfileName());
+        assertEquals(userResponseDTO.getUserId(), result.getUserId());
+        assertEquals(userResponseDTO.getUserProfiles().getProfileName(),
+                result.getUserProfiles().getProfileName());
+        assertEquals(userResponseDTO.getUserPreferences().getWhatsAppOptIn(),
+                result.getUserPreferences().getWhatsAppOptIn());
+        assertEquals(userResponseDTO.getBirthDate(), result.getBirthDate());
+        assertEquals(userResponseDTO.getCpf(),
+                result.getCpf());
     }
 
 }
