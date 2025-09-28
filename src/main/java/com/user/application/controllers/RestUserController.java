e7dd1db8b84214ecf4bba7650956ec44cfa23994
melissasoledade <melissa.gsoledade@gmail.com>
2025-09-28 15:50:09 -0300

    controller init

---
+++ src/main/java/com/user/application/controllers/RestUserController.java
@@ -0,0 +1,46 @@
+package com.user.application.controllers;
+
+import com.user.application.dto.UserDTO;
+import com.user.application.services.UserService;
+import lombok.AllArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.Optional;
+
+@RestController
+@Slf4j
+@AllArgsConstructor
+@RequestMapping("/user")
+public class RestUserController {
+
+    private final UserService userService;
+
+    @GetMapping("/{id}")
+    public ResponseEntity<?> getUser(@PathVariable Long id) {
+        log.info("Getting user information with id {}", id);
+        final Optional<UserDTO> userDTO = userService.getUser(id);
+        log.info("User {} with id: {}", userDTO, id);
+
+        if (userDTO.isEmpty()) {
+            return ResponseEntity.ok("No user found with id");
+        }
+
+        return ResponseEntity.ok(userDTO);
+    }
+
+    @PostMapping
+    public ResponseEntity<?> createUser(@RequestBody UserDTO userDTO) {
+        log.info("Creating user {}", userDTO);
+        Optional<UserDTO> result = this.userService.createUser(userDTO);
+        log.info("User created {}", result);
+
+        if (result.isEmpty()) {
+            return ResponseEntity.ok("User not created");
+        }
+
+        return ResponseEntity.ok(result.get());
+    }
+
+}
---
+++ src/main/java/com/user/application/services/UserService.java
@@ -0,0 +1,32 @@
+package com.user.application.services;
+
+import com.user.application.dto.UserDTO;
+import com.user.application.mappers.UserMapper;
+import com.user.domain.entities.User;
+import com.user.domain.repositories.BaseUserRepository;
+import lombok.AllArgsConstructor;
+import org.springframework.stereotype.Service;
+
+import java.util.Optional;
+
+@Service
+@AllArgsConstructor
+public class UserService {
+
+    private final UserMapper userMapper;
+    private final BaseUserRepository repository;
+
+    public Optional<UserDTO> getUser(Long id) {
+        Optional<User> user = this.repository.findUserById(id);
+
+        return user.map(this.userMapper::fromUser);
+    }
+
+    public Optional<UserDTO> createUser(UserDTO userDTO) {
+        User user = this.userMapper.toUser(userDTO);
+        User savedUser = this.repository.saveUser(user);
+
+        return Optional.of(this.userMapper.fromUser(savedUser));
+    }
+
+}
