a23730fdf04392920996db20c586d5642b6b0977
melissasoledade <melissa.gsoledade@gmail.com>
2025-10-06 23:17:53 -0300

    fix tests

--- src/test/java/com/user/application/controllers/RestUserProfilesControllerIT.java
+++ src/test/java/com/user/application/controllers/RestUserProfilesControllerIT.java
@@ -9,12 +9,15 @@ import com.user.fixtures.domain.UserHelper;
 import com.user.fixtures.domain.UserPreferencesHelper;
 import com.user.fixtures.domain.UserProfilesHelper;
 import jakarta.transaction.Transactional;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.http.MediaType;
 import org.springframework.test.web.servlet.MockMvc;
+
+import java.text.ParseException;
 
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath;
@@ -37,22 +40,25 @@ class RestUserProfilesControllerIT {
     @Autowired
     private ObjectMapper objectMapper;
 
-    @Test
-    void shouldGetUserProfilesById() throws Exception {
-        // given
-        final User user = UserHelper.defaultUser()
+    private User user;
+
+    @BeforeEach
+    void setup() throws ParseException {
+        user = UserHelper.defaultUser()
                 .id(null)
                 .address(AddressHelper.defaultAddress().
                         addressId(null).build())
                 .userPreferences(UserPreferencesHelper.defaultUserPreferences()
                         .userPreferencesId(null).build())
                 .userProfiles(UserProfilesHelper.defaultUserProfiles()
-                        .userProfilesId(null).build())
-                .build();
+                        .userProfilesId(null).build()).build();
         this.repository.saveUser(user);
+    }
 
+    @Test
+    void shouldGetUserProfilesById() throws Exception {
         // when & then
-        this.mvc.perform(get("/user/profiles/" + 1L)
+        this.mvc.perform(get("/user/profiles/" + user.getId().toString())
                         .contentType(MediaType.APPLICATION_JSON))
                 .andExpect(status().isOk())
                 .andExpect(jsonPath("$.profileCode").value("2"))
